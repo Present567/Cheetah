0466598ad5826b3785fe56b75858c176fed6e606
8431137 <8431137@naver.com>
2020-05-06 21:55:07 +0900

    Initial commit

---
+++ app/src/main/java/com/example/teamproject00/RegisterRequest.java
@@ -0,0 +1,24 @@
+package com.example.teamproject00;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import java.util.HashMap;
+import java.util.Map;
+public class RegisterRequest extends StringRequest {
+
+    final static String URL ="http://hong0034.dothome.co.kr/Register.php";
+    private Map<String,String> parameters;
+
+    public RegisterRequest(String userID, String userPassword, String userName, int userAge, Response.Listener<String>listener){
+        super(Method.POST,URL,listener,null);
+        parameters= new HashMap<>();
+        parameters.put("userID",userID);
+        parameters.put("userPassword",userPassword);
+        parameters.put("userName",userName);
+        parameters.put("userAge",userAge+"");
+    }
+    @Override
+    public Map<String,String> getParams(){
+        return parameters;
+    }
+}
